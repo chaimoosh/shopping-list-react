c102363f29ef0a4921768aada6c1586ac2ec806f
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-07-05 20:37:35 -0400

    Configured database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_05_221349) do
+ActiveRecord::Schema.define(version: 2018_07_05_223625) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2018_07_05_221349) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.string "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "notes"
   end
 
 end
